6caf7fc0d8f6207773e82551e1441db3b15cb3a5
nquinbla <nquinbla@myuax.com>
2024-03-31 15:27:50 +0200

    3.1 BuscadorPalabras, comentarios

--- src/main/java/C_BúsquedaYGestión/BuscadorPalabras.java
+++ src/main/java/C_BúsquedaYGestión/BuscadorPalabras.java
@@ -3,8 +3,10 @@ package C_BúsquedaYGestión;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+// Clase que busca una palabra en un texto y devuelve el número de veces que aparece
 public class BuscadorPalabras {
 
+    // Método que busca una palabra en un texto y devuelve el número de veces que aparece
     public static int buscarPalabra(String texto, String palabra) {
         Pattern pattern = Pattern.compile("\\b" + palabra + "\\b", Pattern.CASE_INSENSITIVE);
         Matcher matcher = pattern.matcher(texto);
