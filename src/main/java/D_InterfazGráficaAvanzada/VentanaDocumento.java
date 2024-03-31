c6176196fa7dd8ab40e6d4f854231284a9435df6
nquinbla <nquinbla@myuax.com>
2024-03-31 16:19:22 +0200

    4.2 VentanaDocumento

---
+++ src/main/java/D_InterfazGráficaAvanzada/VentanaDocumento.java
@@ -0,0 +1,17 @@
+package D_InterfazGráficaAvanzada;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class VentanaDocumento extends JInternalFrame {
+
+    public VentanaDocumento() {
+        super("Documento", true, true, true, true);
+        setSize(300, 200);
+
+        JTextArea areaTexto = new JTextArea();
+        JScrollPane scrollPane = new JScrollPane(areaTexto);
+        getContentPane().add(scrollPane, BorderLayout.CENTER);
+    }
+}
+
