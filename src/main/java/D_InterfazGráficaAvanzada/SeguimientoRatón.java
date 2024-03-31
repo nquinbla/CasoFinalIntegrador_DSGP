758e790c954c01a8ee12cee500fabc2202c44556
nquinbla <nquinbla@myuax.com>
2024-03-31 16:22:23 +0200

    4.4 SegumientoRatón

---
+++ src/main/java/D_InterfazGráficaAvanzada/SeguimientoRatón.java
@@ -0,0 +1,32 @@
+package D_InterfazGráficaAvanzada;
+import javax.swing.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class SeguimientoRatón extends JFrame {
+
+    public SeguimientoRatón() {
+        setTitle("Seguimiento del Ratón");
+        setSize(400, 300);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        JLabel etiquetaPosicion = new JLabel("Posición: (0, 0)");
+        getContentPane().add(etiquetaPosicion);
+
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseMoved(MouseEvent e) {
+                etiquetaPosicion.setText("Posición: (" + e.getX() + ", " + e.getY() + ")");
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new SeguimientoRatón().setVisible(true);
+            }
+        });
+    }
+}
