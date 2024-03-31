ae53f01532524bc3c06fbb3b89ab13e238933efc
nquinbla <nquinbla@myuax.com>
2024-03-31 16:18:33 +0200

    4 VentanaPrincipal

--- src/main/java/D_InterfazGráficaAvanzada/VentanaPrincipal.java
+++ src/main/java/D_InterfazGráficaAvanzada/VentanaPrincipal.java
@@ -6,5 +6,60 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class VentanaPrincipal extends JFrame {
-    x
+
+    public VentanaPrincipal() {
+        setTitle("Interfaz Gráfica Avanzada");
+        setSize(800, 600);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        JDesktopPane desktopPane = new JDesktopPane();
+        setContentPane(desktopPane);
+
+        JMenuBar menuBar = new JMenuBar();
+        JMenu menuArchivo = new JMenu("Archivo");
+        JMenuItem itemNuevo = new JMenuItem("Nuevo Documento");
+        JMenuItem itemCerrar = new JMenuItem("Cerrar Todo");
+
+        itemNuevo.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                crearVentanaDocumento();
+            }
+        });
+
+        itemCerrar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                cerrarTodasVentanas();
+            }
+        });
+
+        menuArchivo.add(itemNuevo);
+        menuArchivo.add(itemCerrar);
+        menuBar.add(menuArchivo);
+        setJMenuBar(menuBar);
+    }
+
+    private void crearVentanaDocumento() {
+        VentanaDocumento ventanaDocumento = new VentanaDocumento();
+        getContentPane().add(ventanaDocumento);
+        ventanaDocumento.setVisible(true);
+    }
+
+    private void cerrarTodasVentanas() {
+        JInternalFrame[] ventanas = this.getContentPane().getAllFrames();
+        for (JInternalFrame ventana : ventanas) {
+            ventana.dispose();
+        }
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new VentanaPrincipal().setVisible(true);
+            }
+        });
+    }
 }
+
