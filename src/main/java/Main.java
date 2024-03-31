997b143cd9c40b1e841c829b13ee5baf0359b789
nquinbla <nquinbla@myuax.com>
2024-03-31 16:43:13 +0200

    Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,75 @@
+import A_EditorTextoInteractivo.*;
+import B_ComparadorContadorContenido.*;
+import C_BúsquedaYGestión.*;
+import D_InterfazGráficaAvanzada.*;
+import E_ValidaciónYDiseño.*;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Ejecutar EditorTextoInteractivo
+        EditorTextoGUI editorTextoGUI = new EditorTextoGUI();
+        editorTextoGUI.setVisible(true);
+
+        // Esperar a que el usuario interactúe con la GUI
+        while (editorTextoGUI.isVisible()) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        // Ejecutar ComparadorContenidoGUI
+        ComparadorContenidoGUI comparadorContenidoGUI = new ComparadorContenidoGUI();
+        comparadorContenidoGUI.setVisible(true);
+
+        // Esperar a que el usuario interactúe con la GUI
+        while (comparadorContenidoGUI.isVisible()) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        // Ejecutar BuscadorPalabrasGUI
+        BuscadorPalabrasGUI buscadorPalabrasGUI = new BuscadorPalabrasGUI();
+        buscadorPalabrasGUI.setVisible(true);
+
+        // Esperar a que el usuario interactúe con la GUI
+        while (buscadorPalabrasGUI.isVisible()) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        // Ejecutar VentanaPrincipal
+        VentanaPrincipal ventanaPrincipal = new VentanaPrincipal();
+        ventanaPrincipal.setVisible(true);
+
+        // Esperar a que el usuario interactúe con la GUI
+        while (ventanaPrincipal.isVisible()) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        // Ejecutar ValidadorEmail
+        ValidadorEmail validadorEmail = new ValidadorEmail();
+        validadorEmail.setVisible(true);
+
+        // Esperar a que el usuario interactúe con la GUI
+        while (validadorEmail.isVisible()) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
