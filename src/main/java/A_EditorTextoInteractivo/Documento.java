032e5502e536534601ba9d25aac439c61a08a05d
nquinbla <nquinbla@myuax.com>
2024-03-31 14:25:18 +0200

    1.2 Documento

---
+++ src/main/java/A_EditorTextoInteractivo/Documento.java
@@ -0,0 +1,19 @@
+package A_EditorTextoInteractivo;
+
+public class Documento {
+    private String contenido;
+
+    public Documento() {
+        contenido = "";
+    }
+
+    public String getContenido() {
+        return contenido;
+    }
+
+    public void setContenido(String contenido) {
+        this.contenido = contenido;
+    }
+
+
+}
