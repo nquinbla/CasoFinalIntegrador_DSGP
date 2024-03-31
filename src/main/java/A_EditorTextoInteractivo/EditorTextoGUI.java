4a1f2dfdce7198c37aa043d698501275aab7be45
nquinbla <nquinbla@myuax.com>
2024-03-31 12:49:35 +0200

    1.1 Editor de texto GUI

--- src/main/java/A_EditorTextoInteractivo/EditorTextoGUI.java
+++ src/main/java/A_EditorTextoInteractivo/EditorTextoGUI.java
@@ -5,5 +5,23 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.*;
 
-public class EditorTextoGUI {
+public class EditorTextoGUI extends JFrame {
+    private JTextArea textArea;
+    private JFileChooser fileChooser;
+
+    public EditorTextoGUI() {
+        setTitle("---- Editor de texto interactivo ----");
+        setSize(800, 600);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        textArea = new JTextArea();
+        JScrollPane scrollPane = new JScrollPane(textArea);
+        add(scrollPane, BorderLayout.CENTER);
+
+        JMenuBar menuBar = new JMenuBar();
+        JMenu menuArchivo = new JMenu("Archivo");
+        JMenuItem menuItemGuardar = new JMenuItem("Guardar");
+        JMenuItem menuItemAbrir = new JMenuItem("Abrir");
+    }
+
 }
