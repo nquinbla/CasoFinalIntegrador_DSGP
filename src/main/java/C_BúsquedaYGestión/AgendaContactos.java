81fc4c4612eff9422cb2f8c8e527c04de72e3013
nquinbla <nquinbla@myuax.com>
2024-03-31 15:35:08 +0200

    3.3 AgendaContactos, comentarios y ajustes

--- src/main/java/C_BúsquedaYGestión/AgendaContactos.java
+++ src/main/java/C_BúsquedaYGestión/AgendaContactos.java
@@ -3,21 +3,14 @@ package C_BúsquedaYGestión;
 import java.util.ArrayList;
 import java.util.List;
 
+// Clase que representa una agenda de contactos
 public class AgendaContactos {
     private List<Contacto> contactos;
+    public AgendaContactos() {contactos = new ArrayList<>();}
+    public void agregarContacto(Contacto contacto) {contactos.add(contacto);}
+    public void eliminarContacto(Contacto contacto) {contactos.remove(contacto);}
 
-    public AgendaContactos() {
-        contactos = new ArrayList<>();
-    }
-
-    public void agregarContacto(Contacto contacto) {
-        contactos.add(contacto);
-    }
-
-    public void eliminarContacto(Contacto contacto) {
-        contactos.remove(contacto);
-    }
-
+    // Método que busca un contacto por nombre
     public List<Contacto> buscarContactos(String palabra) {
         List<Contacto> contactosEncontrados = new ArrayList<>();
         for (Contacto contacto : contactos) {
@@ -30,6 +23,7 @@ public class AgendaContactos {
         return contactosEncontrados;
     }
 
+    // Método que busca un contacto por nombre
     public List<Contacto> buscarContactoPorEmail(String email) {
         List<Contacto> resultados = new ArrayList<>();
         for (Contacto contacto : contactos) {
@@ -40,6 +34,7 @@ public class AgendaContactos {
         return resultados;
     }
 
+    // Método que busca un contacto por nombre
     public List<Contacto> buscarContactoPorTelefono(String telefono) {
         List<Contacto> resultados = new ArrayList<>();
         for (Contacto contacto : contactos) {
