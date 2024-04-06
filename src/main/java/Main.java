c24309d592def8dcd057ebc668e2365b84d806ad
nquinbla <nquinbla@myuax.com>
2024-04-06 15:31:28 +0200

    ayuda con el main porfa, ,estoy haciendo 1000 pruebas pasa de esto (ajustes 5)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,12 +4,18 @@ import C_BúsquedaYGestión.*;
 import D_InterfazGráficaAvanzada.*;
 import E_ValidaciónYDiseño.*;
 
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-
-
-
+        SwingUtilities.invokeLater(() -> {
+            try {
+                MainFrame frame = new MainFrame();
+                frame.setVisible(true);
+            } catch (Exception e) {
+                // Manejo de excepciones
+                JOptionPane.showMessageDialog(null, "Ha ocurrido un error al iniciar la aplicación.", "Error", JOptionPane.ERROR_MESSAGE);
+            }
+        });
     }
-}
