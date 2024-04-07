a26cdcc923fe01a4f7a276423fa80289dcf1b11f
nquinbla <nquinbla@myuax.com>
2024-04-07 13:01:19 +0200

    nos e como coño alinear el titulo

--- src/main/java/D_InterfazGráficaAvanzada/MainFrame.java
+++ src/main/java/D_InterfazGráficaAvanzada/MainFrame.java
@@ -19,38 +19,55 @@ public class MainFrame extends JFrame {
 
         // Configurar el diseño del marco principal
         JPanel panelPrincipal = new DecoraciónPanel(); // Usar un panel personalizado
-        BoxLayout boxLayout = new BoxLayout(panelPrincipal, BoxLayout.Y_AXIS);
-        panelPrincipal.setLayout(boxLayout); // Usar BoxLayout
+        panelPrincipal.setLayout(new BorderLayout()); // Usar BorderLayout
         panelPrincipal.setBackground(new Color(255, 255, 255, 200)); // Blanco semi-transparente
         panelPrincipal.setOpaque(false); // Hacer que panelPrincipal sea transparente
         add(panelPrincipal);
+
+        // Panel para el título y los subtítulos
+        JPanel panelTitulo = new JPanel();
+        panelTitulo.setLayout(new BoxLayout(panelTitulo, BoxLayout.Y_AXIS));
+        panelTitulo.setOpaque(false); // Hacer que panelTitulo sea transparente
+        panelPrincipal.add(panelTitulo, BorderLayout.CENTER);
+
+        // Espacio flexible antes del título
+        panelTitulo.add(Box.createVerticalGlue());
 
         // Título Principal "UAX"
         JLabel tituloPrincipal = new JLabel("<html><font color='grey'>U</font><font color='grey'>A</font><font color='blue'>X</font></html>");
         tituloPrincipal.setFont(new Font("Ardela Edge ARDELA EDGE X03 Extra Bold", Font.BOLD, 142));
         tituloPrincipal.setAlignmentX(Component.CENTER_ALIGNMENT); // Centrar el título
-        panelPrincipal.add(Box.createVerticalGlue()); // Espacio entre el título y los subtítulos
-        panelPrincipal.add(tituloPrincipal);
+        panelTitulo.add(tituloPrincipal);
+
+        // Espacio flexible después del título
+        panelTitulo.add(Box.createVerticalGlue());
 
         // Subtítulos "Alfonso X el Sabio" y "Estructuras de datos"
         JLabel subtitulo1 = new JLabel("Universidad Alfonso X el Sabio");
         subtitulo1.setFont(new Font("Touch Me Sans Petite Semi Bold", Font.ITALIC, 24));
         subtitulo1.setForeground(Color.BLACK);
         subtitulo1.setAlignmentX(Component.CENTER_ALIGNMENT); // Centrar el subtítulo
-        panelPrincipal.add(subtitulo1);
+        panelTitulo.add(subtitulo1);
 
         JLabel subtitulo2 = new JLabel("Estructuras de datos");
         subtitulo2.setFont(new Font("Touch Me Sans Petite Semi Bold", Font.BOLD, 24));
         subtitulo2.setForeground(Color.BLUE);
         subtitulo2.setAlignmentX(Component.CENTER_ALIGNMENT); // Centrar el subtítulo
-        panelPrincipal.add(subtitulo2);
-        panelPrincipal.add(Box.createVerticalGlue()); // Espacio entre los subtítulos y el logo
+        panelTitulo.add(subtitulo2);
+        panelTitulo.add(Box.createVerticalGlue()); // Espacio entre los subtítulos y el logo
 
-        // Logo de la Universidad UAX
-        ImageIcon logoIcon = new ImageIcon("C:\\Users\\nerea\\OneDrive\\Imágenes\\logoUAX.png");
-        JLabel logoLabel = new JLabel(logoIcon);
-        logoLabel.setAlignmentX(Component.RIGHT_ALIGNMENT); // Alinear el logo a la derecha
-        panelPrincipal.add(logoLabel);
+        // Panel para el logo
+        JPanel panelLogo = new JPanel(new FlowLayout(FlowLayout.RIGHT)); // Alinear a la derecha
+        panelLogo.setOpaque(false); // Hacer que panelLogo sea transparente
+        panelPrincipal.add(panelLogo, BorderLayout.PAGE_END);
+
+        // Logo de la Universidad UAX (más pequeño)
+        ImageIcon logoIcon = new ImageIcon("C:\\Users\\nerea\\OneDrive\\Imágenes\\logoUAX-removebg-preview.png");
+        Image img = logoIcon.getImage();
+        Image newImg = img.getScaledInstance(logoIcon.getIconWidth() / 3, logoIcon.getIconHeight() / 3, Image.SCALE_SMOOTH);
+        ImageIcon newLogoIcon = new ImageIcon(newImg);
+        JLabel logoLabel = new JLabel(newLogoIcon);
+        panelLogo.add(logoLabel);
 
     }
 
