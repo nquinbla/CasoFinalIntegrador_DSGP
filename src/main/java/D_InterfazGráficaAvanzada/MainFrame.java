40bb6249d9b10432cc12ed10dfc21263bc65bee3
nquinbla <nquinbla@myuax.com>
2024-04-07 13:19:49 +0200

    rio por no llorar

--- src/main/java/D_InterfazGráficaAvanzada/MainFrame.java
+++ src/main/java/D_InterfazGráficaAvanzada/MainFrame.java
@@ -1,7 +1,5 @@
 package D_InterfazGráficaAvanzada;
 
-import D_InterfazGráficaAvanzada.DecoraciónPanel;
-
 import javax.swing.*;
 import java.awt.*;
 
@@ -19,47 +17,52 @@ public class MainFrame extends JFrame {
 
         // Configurar el diseño del marco principal
         JPanel panelPrincipal = new DecoraciónPanel(); // Usar un panel personalizado
-        panelPrincipal.setLayout(new BorderLayout()); // Usar BorderLayout
+        panelPrincipal.setLayout(new GridBagLayout()); // Usar GridBagLayout
         panelPrincipal.setBackground(new Color(255, 255, 255, 200)); // Blanco semi-transparente
         panelPrincipal.setOpaque(false); // Hacer que panelPrincipal sea transparente
         add(panelPrincipal);
 
-        // Panel para el título y los subtítulos
-        JPanel panelTitulo = new JPanel();
-        panelTitulo.setLayout(new BoxLayout(panelTitulo, BoxLayout.Y_AXIS));
-        panelTitulo.setOpaque(false); // Hacer que panelTitulo sea transparente
-        panelPrincipal.add(panelTitulo, BorderLayout.CENTER);
-
-        // Espacio flexible antes del título
-        panelTitulo.add(Box.createVerticalGlue());
-
         // Título Principal "UAX"
         JLabel tituloPrincipal = new JLabel("<html><font color='grey'>U</font><font color='grey'>A</font><font color='blue'>X</font></html>");
         tituloPrincipal.setFont(new Font("Ardela Edge ARDELA EDGE X03 Extra Bold", Font.BOLD, 142));
-        tituloPrincipal.setAlignmentX(Component.CENTER_ALIGNMENT); // Centrar el título
-        panelTitulo.add(tituloPrincipal);
-
-        // Espacio flexible después del título
-        panelTitulo.add(Box.createVerticalGlue());
+        GridBagConstraints gbcTitulo = new GridBagConstraints();
+        gbcTitulo.gridx = 0;
+        gbcTitulo.gridy = 0;
+        gbcTitulo.weighty = 1;
+        gbcTitulo.anchor = GridBagConstraints.PAGE_END;
+        panelPrincipal.add(tituloPrincipal, gbcTitulo);
 
         // Subtítulos "Alfonso X el Sabio" y "Estructuras de datos"
         JLabel subtitulo1 = new JLabel("Universidad Alfonso X el Sabio");
         subtitulo1.setFont(new Font("Touch Me Sans Petite Semi Bold", Font.ITALIC, 24));
         subtitulo1.setForeground(Color.BLACK);
-        subtitulo1.setAlignmentX(Component.CENTER_ALIGNMENT); // Centrar el subtítulo
-        panelTitulo.add(subtitulo1);
+        GridBagConstraints gbcSubtitulo1 = new GridBagConstraints();
+        gbcSubtitulo1.gridx = 0;
+        gbcSubtitulo1.gridy = 1;
+        gbcSubtitulo1.weighty = 0;
+        gbcSubtitulo1.anchor = GridBagConstraints.PAGE_START;
+        panelPrincipal.add(subtitulo1, gbcSubtitulo1);
 
         JLabel subtitulo2 = new JLabel("Estructuras de datos");
         subtitulo2.setFont(new Font("Touch Me Sans Petite Semi Bold", Font.BOLD, 24));
         subtitulo2.setForeground(Color.BLUE);
-        subtitulo2.setAlignmentX(Component.CENTER_ALIGNMENT); // Centrar el subtítulo
-        panelTitulo.add(subtitulo2);
-        panelTitulo.add(Box.createVerticalGlue()); // Espacio entre los subtítulos y el logo
+        GridBagConstraints gbcSubtitulo2 = new GridBagConstraints();
+        gbcSubtitulo2.gridx = 0;
+        gbcSubtitulo2.gridy = 2;
+        gbcSubtitulo2.weighty = 1;
+        gbcSubtitulo2.anchor = GridBagConstraints.PAGE_START;
+        panelPrincipal.add(subtitulo2, gbcSubtitulo2);
 
         // Panel para el logo
         JPanel panelLogo = new JPanel(new FlowLayout(FlowLayout.RIGHT)); // Alinear a la derecha
         panelLogo.setOpaque(false); // Hacer que panelLogo sea transparente
-        panelPrincipal.add(panelLogo, BorderLayout.PAGE_END);
+        GridBagConstraints gbcLogo = new GridBagConstraints();
+        gbcLogo.gridx = 0;
+        gbcLogo.gridy = 3;
+        gbcLogo.weightx = 1.0;
+        gbcLogo.weighty = 1.0;
+        gbcLogo.anchor = GridBagConstraints.LAST_LINE_END;
+        panelPrincipal.add(panelLogo, gbcLogo);
 
         // Logo de la Universidad UAX (más pequeño)
         ImageIcon logoIcon = new ImageIcon("C:\\Users\\nerea\\OneDrive\\Imágenes\\logoUAX-removebg-preview.png");
