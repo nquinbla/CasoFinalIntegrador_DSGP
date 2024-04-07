3f9021689ef1bef1cac13010177f0e9da2a997e5
nquinbla <nquinbla@myuax.com>
2024-04-07 12:47:06 +0200

    Arreglos >:/ arreglillos raros

--- src/main/java/D_InterfazGráficaAvanzada/DecoraciónPanel.java
+++ src/main/java/D_InterfazGráficaAvanzada/DecoraciónPanel.java
@@ -11,25 +11,26 @@ public class DecoraciónPanel extends JPanel {
         int width = getWidth();
         int height = getHeight();
 
-        Color colorlinea = new Color(54, 121, 250, 98); // Blanco semi-transparente
+        Color colorlínea = new Color(54, 121, 250, 98); // Blanco semi-transparente
 
         // Dibujar la primera línea inclinada de izquierda a derecha
-        g2d.setColor(colorlinea);
+        g2d.setColor(colorlínea);
         g2d.drawLine(width / 3, 0, 0, height);
 
+        // Dibujar la segunda línea inclinada de izquierda a derecha equidistante de la primera
+        int d = 225; // Distancia de la nueva línea a la primera
+        g2d.drawLine(width / 3 + d, 0 + d, 0 + d, height + d);
+
         // Dibujar un triángulo entre las dos líneas
-        int[] xPoints = {0, width / 3, width * 2 / 3}; // Coordenadas x de los puntos del triángulo
-        int[] yPoints = {height, 0, 0}; // Coordenadas y de los puntos del triángulo
+        int[] xPoints = {0, width / 3, width * 2 / 3}; // Coordenadas "x" de los puntos del triángulo
+        int[] yPoints = {height, 0, 0}; // Coordenadas "y" de los puntos del triángulo
         int nPoints = 3; // Número de puntos del triángulo
 
         // Rellenar el triángulo con un color semi-transparente
-        Color colortriangulos = new Color(54, 121, 250, 98); // Azul semi-transparente
-        g2d.setColor(colortriangulos);
+        Color colortriángulos = new Color(54, 121, 250, 98); // Azul semi-transparente
+        g2d.setColor(colortriángulos);
         g2d.fillPolygon(xPoints, yPoints, nPoints); // Dibujar y rellenar el triángulo
 
-        // Otra línea inclinada pero más pequeña
-        int[] xPoints2 = {width * 2 / 3, width * 3 / 4, width}; // Coordenadas x de los puntos del segundo triángulo
-        int[] yPoints2 = {0, height / 2, 0}; // Coordenadas y de los puntos del segundo triángulo
-        g2d.fillPolygon(xPoints2, yPoints2, nPoints); // Dibujar y rellenar el segundo triángulo
+
     }
 }
