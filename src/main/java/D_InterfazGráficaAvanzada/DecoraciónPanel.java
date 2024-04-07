760f3426d7e57e98da6b2fa00e11ae3cfcb935de
nquinbla <nquinbla@myuax.com>
2024-04-07 11:33:50 +0200

    decorando 8

--- src/main/java/D_InterfazGráficaAvanzada/DecoraciónPanel.java
+++ src/main/java/D_InterfazGráficaAvanzada/DecoraciónPanel.java
@@ -10,18 +10,14 @@ public class DecoraciónPanel extends JPanel {
         int width = getWidth();
         int height = getHeight();
 
-        // Dibujar los rectángulos blancos en las partes superior e inferior
-        g2d.setColor(Color.WHITE);
-        g2d.fillRect(0, 0, width, height / 3);
-        g2d.fillRect(0, 2 * height / 3, width, height / 3);
-
-        // Dibujar los rectángulos azules en la parte media
-        g2d.setColor(Color.BLUE);
-        g2d.fillRect(0, height / 3, width, height / 3);
-
         // Dibujar las líneas inclinadas para delimitar los colores
-        g2d.setColor(Color.BLACK);
-        g2d.drawLine(0, 0, width, height);
-        g2d.drawLine(width, 0, 0, height);
+        g2d.setColor(Color.BLUE);
+        g2d.drawLine(width / 3, 0, 0, height); // Línea inclinada de izquierda a derecha
+        g2d.drawLine(2 * width / 3, 0, width, height); // Línea inclinada de derecha a izquierda
+
+        // Dibujar un triángulo inclinado entre las líneas
+        int[] xPoints = {width / 3, 2 * width / 3, width / 2};
+        int[] yPoints = {0, 0, height};
+        g2d.fillPolygon(xPoints, yPoints, 3); // Triángulo inclinado entre las líneas
     }
 }
