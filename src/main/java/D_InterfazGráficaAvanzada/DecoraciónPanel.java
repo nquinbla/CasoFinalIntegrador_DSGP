a73eae12b3486221bd9ed39450b27d58efd12e49
nquinbla <nquinbla@myuax.com>
2024-04-07 11:31:36 +0200

    decorando 7

--- src/main/java/D_InterfazGráficaAvanzada/DecoraciónPanel.java
+++ src/main/java/D_InterfazGráficaAvanzada/DecoraciónPanel.java
@@ -10,16 +10,18 @@ public class DecoraciónPanel extends JPanel {
         int width = getWidth();
         int height = getHeight();
 
-        // Dibujar el rectángulo azul en la mitad izquierda
-        g2d.setColor(Color.BLUE);
-        g2d.fillRect(0, 0, width / 2, height);
-
-        // Dibujar el rectángulo blanco en la mitad derecha
+        // Dibujar los rectángulos blancos en las partes superior e inferior
         g2d.setColor(Color.WHITE);
-        g2d.fillRect(width / 2, 0, width / 2, height);
+        g2d.fillRect(0, 0, width, height / 3);
+        g2d.fillRect(0, 2 * height / 3, width, height / 3);
 
-        // Dibujar la línea inclinada para delimitar los colores
+        // Dibujar los rectángulos azules en la parte media
+        g2d.setColor(Color.BLUE);
+        g2d.fillRect(0, height / 3, width, height / 3);
+
+        // Dibujar las líneas inclinadas para delimitar los colores
         g2d.setColor(Color.BLACK);
-        g2d.drawLine(width / 1, 0, 0, height); // Línea inclinada de izquierda a derecha
+        g2d.drawLine(0, 0, width, height);
+        g2d.drawLine(width, 0, 0, height);
     }
 }
